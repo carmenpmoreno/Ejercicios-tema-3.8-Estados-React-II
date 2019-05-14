75ad025b7a8122f7c8431b173560923a654bd32b
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-14 16:59:00 +0200

    exercise 3 completed

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ class App extends React.Component {
       name: "",
       description: "",
       language: "",
+      clasification: "",
+      genres: [],
     }
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleGenreChange = this.handleGenreChange.bind(this);
   }
 
   handleInputChange(event) {
@@ -17,6 +20,19 @@ class App extends React.Component {
     this.setState({
       [text]: event.target.value,
     })
+  }
+  handleGenreChange(event){
+    const genre = event.target.value;
+    let arr = this.state.genres;
+    if(arr.length===3){
+      alert('Se ha alcanzado el máx de géneros')
+    }
+    else{
+    arr.push(genre);
+    this.setState({
+      genres : arr
+    })
+  }
   }
 
   render() {
@@ -44,11 +60,34 @@ class App extends React.Component {
             <option value="english">english</option>
             <option value="portuguese">portuguese</option>
           </select>
+          <h2>Clasification</h2>
+          <label>A</label>
+          <input type="radio" onChange = {this.handleInputChange} name="clasification" value="a" checked/>
+          <label>7</label>
+          <input type="radio" onChange = {this.handleInputChange} name="clasification" value="7"/>
+          <label>12</label>
+          <input type="radio" onChange = {this.handleInputChange} name="clasification" value="12" />
+          <label>16</label>
+          <input type="radio" onChange = {this.handleInputChange} name="clasification" value="16"/>
+          <label>18</label>
+          <input type="radio" onChange = {this.handleInputChange} name="clasification" value="18"/>
+
+          <input type="checkbox" onChange = {this.handleGenreChange} name="genre" value="comedy"/>
+          <label>comedy</label>
+          <input type="checkbox" onChange = {this.handleGenreChange} name="genre" value="drama"/>
+          <label>drama</label>
+          <input type="checkbox" onChange = {this.handleGenreChange} name="genre" value="horror"/>
+          <label>horror</label>
+          <input type="checkbox" onChange = {this.handleGenreChange} name="genre" value="fantasy"/>
+          <label>fantasy</label>
+          <input type="checkbox" onChange = {this.handleGenreChange} name="genre" value="action"/>
+          <label>action</label>
         </form>
         <div className="card">
           <h1 className="name">{this.state.name}</h1>
           <p className="description">{this.state.description}</p>
           <p className="language">{this.state.language}</p>
+          <p className="clasification">{this.state.clasification}</p>
         </div>
       </div>
     );
